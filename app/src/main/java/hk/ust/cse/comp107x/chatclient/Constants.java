94dab7f639d61ba3fe851b5da406116b7d101544
zhinieing <799759861@qq.com>
2017-10-14 15:38:05 +0800

    Initial commit

---
+++ app/src/main/java/hk/ust/cse/comp107x/chatclient/Constants.java
@@ -0,0 +1,11 @@
+package hk.ust.cse.comp107x.chatclient;
+
+/**
+ * Created by muppala on 18/6/16.
+ */
+public class Constants {
+
+    public static final String SERVER = "http://117.48.197.65/";
+
+    public static final String JSON_URL = SERVER + "people.json";
+}
